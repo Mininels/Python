fcdf3d7afe00291863c48f0f7ee515532d7736e1
felipe.mininel <felipe351@gmail.com>
2022-10-14 11:06:19 -0300

    Criacao de arquivos

---
+++ Python/receitas/settings.py
@@ -0,0 +1,11 @@
+#aplication definition 
+
+INSTALLED_APPS = [
+    'receitas',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles'
+]
+
